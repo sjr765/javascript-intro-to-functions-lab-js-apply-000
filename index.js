4ef5bdd9e914fedb91d0567caafcf0a550dade4e
sjr765 <sjr765@gmail.com>
2018-05-31 21:02:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string){
   console.log(string.toLowerCase());
 }
 
-function sayHiToGrandmaUper(string){
+function sayHiToGrandma(string){
   if (string === string.toLowerCase() ){
       return "I can't hear you!"
   } 
