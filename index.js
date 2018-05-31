d7831b710404772cb3d7ddb2cc7c7021d1b8cab6
sjr765 <sjr765@gmail.com>
2018-05-31 21:02:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,17 +17,12 @@ function logWhisper(string){
 function sayHiToGrandmaUper(string){
   if (string === string.toLowerCase() ){
       return "I can't hear you!"
-  }
-}
-
-function sayHiToGrandmaLower(string){
-  if (string === string.toUpperCase() ){
+  } 
+  else if (string === string.toUpperCase() ){
       return "YES INDEED!"
-  }
-}
 
-function sayHiToGrandma(string){
-  if ( string === "I love you, Grandma."){
+  }
+  else if ( string === "I love you, Grandma."){
       return "I love you, too."
   }
 }
